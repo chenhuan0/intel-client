64b9b44e0b9b505cbd39048ef45a36895b5b080d
Chen Huan <chenhuan0@gmail.com>
2013-03-07 15:23:33 +0800

    添加图片

--- mainwindow_ui.h
+++ mainwindow_ui.h
@@ -1,7 +1,7 @@
 /********************************************************************************
 ** Form generated from reading UI file 'mainwindow.ui'
 **
-** Created: Thu Mar 7 14:50:00 2013
+** Created: Thu Mar 7 15:21:41 2013
 **      by: Qt User Interface Compiler version 4.8.4
 **
 ** WARNING! All changes made in this file will be lost when recompiling UI file!
@@ -14,12 +14,9 @@
 #include <QtGui/QAction>
 #include <QtGui/QApplication>
 #include <QtGui/QButtonGroup>
-#include <QtGui/QHBoxLayout>
 #include <QtGui/QHeaderView>
 #include <QtGui/QLabel>
 #include <QtGui/QMainWindow>
-#include <QtGui/QSpacerItem>
-#include <QtGui/QVBoxLayout>
 #include <QtGui/QWidget>
 
 QT_BEGIN_NAMESPACE
@@ -28,17 +25,7 @@ class Ui_mainWindow
 {
 public:
     QWidget *centralwidget;
-    QWidget *widget;
-    QVBoxLayout *verticalLayout;
-    QHBoxLayout *horizontalLayout;
-    QSpacerItem *horizontalSpacer;
     QLabel *label;
-    QSpacerItem *horizontalSpacer_2;
-    QSpacerItem *verticalSpacer;
-    QHBoxLayout *horizontalLayout_2;
-    QSpacerItem *horizontalSpacer_3;
-    QLabel *label_2;
-    QSpacerItem *horizontalSpacer_4;
 
     void setupUi(QMainWindow *mainWindow)
     {
@@ -49,52 +36,10 @@ public:
         mainWindow->setMaximumSize(QSize(720, 480));
         centralwidget = new QWidget(mainWindow);
         centralwidget->setObjectName(QString::fromUtf8("centralwidget"));
-        widget = new QWidget(centralwidget);
-        widget->setObjectName(QString::fromUtf8("widget"));
-        widget->setGeometry(QRect(100, 20, 481, 150));
-        verticalLayout = new QVBoxLayout(widget);
-        verticalLayout->setObjectName(QString::fromUtf8("verticalLayout"));
-        verticalLayout->setContentsMargins(0, 0, 0, 0);
-        horizontalLayout = new QHBoxLayout();
-        horizontalLayout->setObjectName(QString::fromUtf8("horizontalLayout"));
-        horizontalSpacer = new QSpacerItem(40, 20, QSizePolicy::Expanding, QSizePolicy::Minimum);
-
-        horizontalLayout->addItem(horizontalSpacer);
-
-        label = new QLabel(widget);
+        label = new QLabel(centralwidget);
         label->setObjectName(QString::fromUtf8("label"));
-
-        horizontalLayout->addWidget(label);
-
-        horizontalSpacer_2 = new QSpacerItem(40, 20, QSizePolicy::Expanding, QSizePolicy::Minimum);
-
-        horizontalLayout->addItem(horizontalSpacer_2);
-
-
-        verticalLayout->addLayout(horizontalLayout);
-
-        verticalSpacer = new QSpacerItem(20, 40, QSizePolicy::Minimum, QSizePolicy::Expanding);
-
-        verticalLayout->addItem(verticalSpacer);
-
-        horizontalLayout_2 = new QHBoxLayout();
-        horizontalLayout_2->setObjectName(QString::fromUtf8("horizontalLayout_2"));
-        horizontalSpacer_3 = new QSpacerItem(40, 20, QSizePolicy::Expanding, QSizePolicy::Minimum);
-
-        horizontalLayout_2->addItem(horizontalSpacer_3);
-
-        label_2 = new QLabel(widget);
-        label_2->setObjectName(QString::fromUtf8("label_2"));
-
-        horizontalLayout_2->addWidget(label_2);
-
-        horizontalSpacer_4 = new QSpacerItem(40, 20, QSizePolicy::Expanding, QSizePolicy::Minimum);
-
-        horizontalLayout_2->addItem(horizontalSpacer_4);
-
-
-        verticalLayout->addLayout(horizontalLayout_2);
-
+        label->setGeometry(QRect(150, 40, 391, 131));
+        label->setPixmap(QPixmap(QString::fromUtf8(":/images/logo.JPG")));
         mainWindow->setCentralWidget(centralwidget);
 
         retranslateUi(mainWindow);
@@ -105,8 +50,7 @@ public:
     void retranslateUi(QMainWindow *mainWindow)
     {
         mainWindow->setWindowTitle(QApplication::translate("mainWindow", "MainWindow", 0, QApplication::UnicodeUTF8));
-        label->setText(QApplication::translate("mainWindow", "<html><head/><body><p><span style=\" font-size:36pt; color:#ff0000;\">\347\247\221\346\225\231\344\277\241\346\201\257\345\217\221\345\270\203\347\263\273\347\273\237</span></p></body></html>", 0, QApplication::UnicodeUTF8));
-        label_2->setText(QApplication::translate("mainWindow", "<html><head/><body><p><span style=\" font-size:16pt; font-style:italic;\">\350\257\267\345\234\250\344\270\213\346\226\271\347\202\271\345\207\273\351\200\211\346\213\251\346\202\250\350\246\201\350\277\233\350\241\214\347\232\204\346\223\215\344\275\234</span></p></body></html>", 0, QApplication::UnicodeUTF8));
+        label->setText(QString());
     } // retranslateUi
 
 };
