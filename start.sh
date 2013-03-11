b5caff8757093df61e5372676b78c3f15ec9e498
Chen Huan <chenhuan0@gmail.com>
2013-03-11 15:08:19 +0800

    删除脚本

--- start.sh
+++
@@ -1,7 +0,0 @@
-#amixer cset name='PCM Playback Volume' 100%,100%
-export FRAMEBUFFER=/dev/fb2
-export QWS_DISPLAY=LinuxFB:/dev/fb2
-export QT_QWS_FONTDIR=/usr/local/Trolltech/QtEmbedded-4.6.2-arm/lib/fonts
-./client -qws
-
-
